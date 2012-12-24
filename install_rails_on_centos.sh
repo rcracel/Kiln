df94a7cd408743834731a1781c20f7f576c420ee
rcracel <roger.cracel@gmail.com>
2012-12-24 03:15:56 -0500

    fixing passanger config setting

--- install_rails_on_centos.sh
+++ install_rails_on_centos.sh
@@ -1,5 +1,49 @@
-sudo adduser deploy
+yum update -y
 
-sudo apt-get -y update
-sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-dev libyaml-dev libcurl4-openssl-dev curl git-core python-software-properties
+yum groupinstall "Development Tools"
+
+yum install rubygems
+
+useradd -m -s /bin/bash rails
+
+su - rails
+
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+
+exec $SHELL -l
+
+
+rbenv install 1.9.3-p327
+
+rbenv rehash
+
+rbenv global 1.9.3-p327
+
+
+exit # back to root
+
+yum install rubygems
+
+gem install rubygems-update
+
+update_rubygems
+
+gem install rake
+gem install bundler
+
+rbenv rehash
+
+git clone https://github.com/rails/rails.git
+cd rails
+$ bundle
+
+# follow instructions on http://wiki.nginx.org/Install
+
+gem install passenger
+
+passenger-install-nginx-module
 
