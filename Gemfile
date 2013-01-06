455cd62d76d1d630752a903d51beda515efb46c8
rcracel <roger.cracel@gmail.com>
2013-01-06 16:49:27 -0500

    adding to dashboard some example graphs

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ end
 
 gem 'jquery-rails'
 
+# gem 'gchart'
+
 gem 'mongo_mapper'
 
 gem 'libv8', '~> 3.11.8'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,54 @@
 class WelcomeController < ApplicationController
 
-  def index
-    
-  end
+    def index
+        @chart_data = {}
+
+        visible_apps    = Application.visible_by_user( current_user )
+                                     .fields( :id, :name )
+
+        @chart_data[ :event_count_for_app ]    = {}
+        @chart_data[ :events_count_by_module ] = {}
+        @chart_data[ :events_count_by_level ]  = {}
+
+        visible_apps_ids = visible_apps.collect { |a| a.id }
+
+        counter = Event.collection.group(
+            [ :application_id, :module_name, :log_level ],
+            { :application_id => { :$in => visible_apps_ids } },
+            { :count => 0 },
+            "function( doc, prev ) { prev.count++ }",
+            true
+        )
+
+        counter.each do |g|
+            app   = visible_apps.select.find { |a| a.id.to_s == g['application_id'].to_s }
+            count = g['count']
+
+            if @chart_data[ :event_count_for_app ][ app.name ].nil?
+                @chart_data[ :event_count_for_app ][ app.name ] = 0
+            end
+            @chart_data[ :event_count_for_app ][ app.name ] += count
+
+            module_name = g['module_name']
+
+            if @chart_data[ :events_count_by_module ][ module_name ].nil?
+                @chart_data[ :events_count_by_module ][ module_name ] = count
+            else
+                @chart_data[ :events_count_by_module ][ module_name ] += count
+            end
+
+            log_level = g['log_level']
+                
+            if @chart_data[ :events_count_by_level ][ log_level ].nil?
+                @chart_data[ :events_count_by_level ][ log_level ] = count
+            else
+                @chart_data[ :events_count_by_level ][ log_level ] += count
+            end
+
+        end
+
+        logger.info "---- #{@chart_data[ :events_count_by_module ]} ----"
+
+    end
 
 end
